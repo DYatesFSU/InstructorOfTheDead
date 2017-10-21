4f2f2d4e3bc0a808cd98031e8e81a387662c285d
harkaranbrar <harkaranbrar7@gmail.com>
2017-10-21 11:03:26 -0700

    Graduation Cap
    Now we have a gaduation cap instead of Sphere ball

--- Source/InstructorOfTheDead/my_projectile.cpp
+++ Source/InstructorOfTheDead/my_projectile.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "my_projectile.h"
+#include "Engine.h"
 
 
 // Sets default values
@@ -9,14 +10,16 @@ Amy_projectile::Amy_projectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+
 }
 
 // Called when the game starts or when spawned
 void Amy_projectile::BeginPlay()
 {
 	Super::BeginPlay();
-    
 	
+
+ 
 }
 
 // Called every frame
